1147552cacff75e25aaff14c27a4b45c25de04ba
Martin Hallén <marthall@outlook.com>
2014-01-14 14:18:26 +0100

    Added html file and exercise is now working

--- oving1/server.py
+++ oving1/server.py
@@ -1,32 +1,31 @@
-import socket module
-from socket import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#import socket module
+from socket import socket, AF_INET, SOCK_STREAM
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-#Prepare a sever socket
-#Fill in start
-#Fill in end
+serverSocket.bind(('', 8081))
+serverSocket.listen(1)
 
 while True:
-    #Establish the connection
     print 'Ready to serve...'
-    connectionSocket, addr = #Fill in start #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
-        message = #Fill in start #Fill in end
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start #Fill in end
-        #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
-        #Send the content of the requested file to the client
+        outputdata = f.read()
+
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
+
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i])
-            connectionSocket.close()
     except IOError:
-    #Send response message for file not found
-#Fill in start
-#Fill in end
-#Close client socket
-#Fill in start
-#Fill in end
+
+        connectionSocket.send('file not found')
+    finally:
+        connectionSocket.close()
+
+
+serverSocket.close()
